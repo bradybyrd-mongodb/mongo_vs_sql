998cc67aaa1c3c6b8b9a219bae114b7dcbd4237b
Brady Byrd <brady.byrd@mongodb.com>
2025-02-06 13:44:40 -0500

    update env script

--- env_setup_deb.sh
+++ env_setup_deb.sh
@@ -5,7 +5,7 @@ sudo apt install -y git
 #git clone git@github.com:bradybyrd-mongodb/mongo_vs_sql.git
 git clone https://github.com/bradybyrd-mongodb/mongo_vs_sql.git
 cd mongo_vs_sql
-echo "# --- python version --- #"
+echo "# --- Python Version --- #"
 # (must be > 3.10)
 python3 --version 
 sudo apt install -y python3.11-venv
@@ -14,4 +14,10 @@ sudo apt install python3-pip -y
 #python3 -m venv .venv --system-site-packages
 python3 -m venv .venv --system-site-packages
 source .venv/bin/activate
+echo "# --- Install Python Packages --- #"
 python3 -m pip install -r requirements.txt
+echo "# -------------------- INFO --------------------------- #"
+echo "  Remember to set an environment variable for your passwords"
+echo "  export _PWD_=<my mongodb password>"
+echo "  export _PGPWD_=<my postgres password>"
+cd mongo_vs_sql
